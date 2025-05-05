37a2a20845bb57f36a0d1a021be09f6b613ad521
Saki Tojo <haolei.ye@anu.edu.au>
2025-05-05 12:53:27 +0800

    Support multiple inference engine.

--- furina/launcher.py
+++ furina/launcher.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
+import importlib
 import os
-from .engine import llama_cpp
 from .lib import model
 
-def run_server(port: int, model_name: str, num_of_params: str = '', quantization: str='',
+
+def run_server(port: int, engine_name: str,
+               model_name: str, num_of_params: str = '', quantization: str='',
                force_cpu_only=None, fource_gpu_type=None):
     # Calculate the model paths.
     local_model_path: str = model.get_local_path(model_name, num_of_params, quantization)
@@ -11,6 +13,12 @@ def run_server(port: int, model_name: str, num_of_params: str = '', quantization
     if not os.path.isfile(local_model_path):
         # Download the model.
         model.download(model_name, num_of_params, quantization)
-    llama_cpp.run_server(port, local_model_path,
-                         force_cpu_only=force_cpu_only,
-                         fource_gpu_type=fource_gpu_type)
+    # Try to import the engine to use.
+    try:
+        target_engine = importlib.import_module(f"furina.engine.{engine_name}")
+    except Exception:
+        raise RuntimeError(f"Failed to load engine {engine_name}")
+    # Launch the engine server.
+    target_engine.run_server(port, local_model_path,
+                             force_cpu_only=force_cpu_only,
+                             fource_gpu_type=fource_gpu_type)
--- simulator.py
+++ simulator.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from furina import launcher
 
-launcher.run_server(port=8080,
-                    model_name="Qwen3",
-                    num_of_params="0.6B",)
+launcher.run_server(port=8080, engine_name="llama_cpp",
+                    model_name="Qwen3", num_of_params="0.6B",)
